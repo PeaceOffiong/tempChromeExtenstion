4440265727e162dceb762e2781ff0869478a6018
peaceOffiong <peaceyben@gamil.com>
2023-10-01 15:52:49 -0700

    bugs fixed

--- js/popup.js
+++ js/popup.js
@@ -1,38 +1,38 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
+  
   const videoStartButton = document.querySelector("button#videoStart");
-  const stopVideo = document.querySelector("button#videoStop");
+  const stopVideo = document.querySelector("button#videoStop")
+    
 
-  videoStartButton.addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        { action: "request_recording" },
-        function (response) {
-          if (!chrome.runtime.lastError) {
-            console.log(response);
-          } else {
-            console.log(chrome.runtime.lastError, "error line 14");
+    videoStartButton.addEventListener("click", () => {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          { action: "request_recording" },
+          function (response) {
+            if (!chrome.runtime.lastError) {
+              console.log(response);
+            } else {
+              console.log(chrome.runtime.lastError, "error line 14");
+            }
           }
-        }
-      );
+        );
+      });
     });
-  });
-
-  stopVideo.addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        { action: "stopvideo" },
-        function (response) {
-          if (!chrome.runtime.lastError) {
-            console.log(response);
-          } else {
-            console.log(chrome.runtime.lastError, "error line 27");
-          }
-        }
-      );
-    });
-  });
-});
+  
+ stopVideo.addEventListener("click", () => {
+   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+     chrome.tabs.sendMessage(
+       tabs[0].id,
+       { action: "stopvideo" },
+       function (response) {
+         if (!chrome.runtime.lastError) {
+           console.log(response);
+         } else {
+           console.log(chrome.runtime.lastError, "error line 27");
+         }
+       }
+     );
+   });
+ });
+})
